8bdf33ff8094af039684a90164a2f415b6cf390c
K-G-C-N-Kulathunga <161619772+K-G-C-N-Kulathunga@users.noreply.github.com>
2025-12-30 12:13:24 +0530

    a

--- Backend/sps/src/main/java/com/it/sps/repository/WiringLandDetailRepository.java
+++ Backend/sps/src/main/java/com/it/sps/repository/WiringLandDetailRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface WiringLandDetailRepository extends JpaRepository<WiringLandDetail, WiringLandDetailPK> {
-
     WiringLandDetail findByIdApplicationIdAndIdDeptId(String applicationId, String deptId);
+
+//    WiringLandDetail findFirstByIdApplicationId(String applicationId);
 
     @Query(value = "SELECT * FROM wiring_land_detail wld WHERE wld.application_id = :applicationId AND ROWNUM = 1", nativeQuery = true)
     WiringLandDetail findFirstByApplicationIdNative(@Param("applicationId") String applicationId);
